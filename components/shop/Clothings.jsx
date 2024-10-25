8a8085afeecc5cd39b034802ac88fb8706b8f843
Jonas <jonaselemented@gmail.com>
2024-10-25 14:17:02 -0700

    new commit

--- components/shop/Clothings.jsx
+++ components/shop/Clothings.jsx
@@ -1,26 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 function Clothings({ data }) {
-  // const [selectedItem, setSelectedItem] = useState(null);
-
-  // useEffect(() => {
-  //   if (data && data.length > 0) {
-  //     const randomIndex = Math.floor(Math.random() * data.length);
-  //     setSelectedItem(data[randomIndex]);
-  //   }
-  // }, [data]);
-
-  // if (!selectedItem) return null;
-
-  // const videoUrl = selectedItem.images?.find(
-  //   (media) => media.type === "video"
-  // )?.url;
+  
+  const videoUrl = data.find()
 
   return (
     <div className="relative rounded-2xl w-full h-full overflow-hidden">
       <div className="w-full h-full relative">
         <video
-          src={data?.images?.[1]?.url} // Assuming there's a `videoUrl` property
+          src={data?.images?.[1]?.url}
           autoPlay
           loop
           muted
@@ -33,9 +21,9 @@ function Clothings({ data }) {
         <div className="absolute bottom-4 left-4 text-white">
           <div className="mt-3 flex flex-col space-y-4">
             <span className="w-20 flex justify-center border-white hover:bg-black hover:text-white hover:border-black text-xs rounded-full border-[1px] px-2 py-1 font-medium">
-              sdfs
+              {data?.tag}
             </span>
-            <h1 className="font-medium">zcsdfs</h1>
+            <h1 className="font-medium">{data?.title}</h1>
           </div>
         </div>
       </div>
