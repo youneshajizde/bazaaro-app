fbd324465d61aedcfa597f7c3d0cfdad9b1d1262
Jonas <jonaselemented@gmail.com>
2024-11-02 12:45:52 -0700

    footer in progress

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,31 +1,28 @@
 import Image from "next/image";
 import React from "react";
-import footerImg from "@/public/assets/footer.jpg";
 import logo from "@/public/assets/log.png";
-
+import cartImg from "@/public/assets/cart.webp";
+import { MoveUp } from "lucide-react";
 function Footer() {
   return (
-    <section className="relative w-full h-[550px] mt-14 overflow-hidden bg-cover bg-center">
-      <div className="absolute inset-0 bg-blue-800 opacity-80 clip-semi-oval">
-        <video
-          src={"/videos/v6.mp4"}
-          autoPlay
-          loop
-          muted
-          className="absolute w-full h-full object-cover"
-        />
+    <footer className="relative w-full mx-auto h-[600px] mt-20 overflow-hidden border-t-[1px] border-gray-300">
+      <div className="w-full flex h-[20%] ">
+        <div className="w-[90%] mx-auto flex items-center justify-between">
+          <h1 className="text-3xl mt-2 font-medium flex items-center justify-between">
+            2025
+          </h1>
+
+          <MoveUp size={40} className="bg-black text-white rounded-full p-2" />
+        </div>
       </div>
 
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white ">
-        <div className="flex items-center gap-2">
-          <Image src={logo} width={30} height={30} alt="Logo" />
-          <h2 className="text-2xl font-semibold logos-text">Bazaaro</h2>
-        </div>
-        <p className="text-center mt-2 font-medium">
-          Discover the best deals and unique products on Bazaaro.
+      <div className="flex h-[80%] w-[90%] mx-auto flex-col">
+        <p className="text-sm font-light">Have a project in mind?</p>
+        <p className="text-center font-medium text-[10rem] text-gray-300 mt-10">
+          LET'S TALK
         </p>
       </div>
-    </section>
+    </footer>
   );
 }
 
